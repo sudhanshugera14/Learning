ac45a7b5b4c69f981564471f33731bd90914ee94
Sudhanshu Gera (Digital) <SU20026350@wipro.com>
2018-10-05 11:13:35 +0530

    First Commit

---
+++ src/com/sudhanshu/SqServlet.java
@@ -0,0 +1,17 @@
+package com.sudhanshu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
+		PrintWriter out = res.getWriter();
+		int k = (int)req.getAttribute("k");
+		int ans = k*k;
+		out.println(ans);
+	}
+}
